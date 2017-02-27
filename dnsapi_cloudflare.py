0224d081b89efe22de2a0365bbd5b5a0d59ca50d
Zachary Schneider <hello@zacharyschneider.ca>
2017-02-26 22:04:24 -0800

    Updated dnsapi_cloudflare.py to use official Cloudflare API.

--- dnsapi_cloudflare.py
+++ dnsapi_cloudflare.py
@@ -16,16 +16,12 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Uses the 'requests' package.
-
 # Requires:
 # dnsapi_data[0]        : Global API key
 # dnsapi_data[1]        : Email address
 # dnsapi_domain_data[0] : Zone ID
 # dnsapi_domain_data[1] : TTL in seconds, automatic if not specified
 # key_data['plain']     : TXT record value in plain unquoted format
-
-# POST URL: https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records
 
 # Parameters:
 # type    : 'TXT'
@@ -34,22 +30,23 @@
 # ttl     : dnsapi_domain_data[1]
 
 import logging
-import requests
-import json
 
-def update( dnsapi_data, dnsapi_domain_data, key_data, debugging = False ):
+import CloudFlare
+
+
+def update(dnsapi_data, dnsapi_domain_data, key_data, debugging=False):
     if len(dnsapi_data) < 2:
-        logging.error( "DNS API cloudflare: API credentials not configured" )
+        logging.error("DNS API Cloudflare: API credentials not configured")
         return False;
     api_key = dnsapi_data[0]
     email = dnsapi_data[1]
     if len(dnsapi_domain_data) < 1:
-        logging.error( "DNS API cloudflare: domain data does not contain zone ID" )
+        logging.error("DNS API Cloudflare: domain data does not contain zone ID")
         return False
     zone_id = dnsapi_domain_data[0]
     if len(dnsapi_domain_data) > 1:
         try:
-            ttl = int( dnsapi_domain_data[1] )
+            ttl = int(dnsapi_domain_data[1])
             if ttl < 1:
                 ttl = 1
         except Exception:
@@ -61,33 +58,30 @@ def update( dnsapi_data, dnsapi_domain_data, key_data, debugging = False ):
         data = key_data['plain']
         domain_suffix = key_data['domain']
     except KeyError as e:
-        logging.error( "DNS API cloudflare: required information not present: %s", str(e) )
+        logging.error("DNS API Cloudflare: required information not present: %s", str(e))
         return False
     if debugging:
         return True
 
-    result = False
-    endpoint = "https://api.cloudflare.com/client/v4/zones/{0}/dns_records".format( zone_id )
-    hdr = { 'Content-Type': 'application/json',
-            'X-Auth-Key': api_key,
-            'X-Auth-Email': email }
-    body = { 'type': 'TXT',
-             'name': selector + '._domainkey.' + domain_suffix,
-             'content': data,
-             'ttl': ttl }
-    resp = requests.post( endpoint, json = body, headers = hdr )
-    logging.info( "HTTP status: %d", resp.status_code )
+    cf = CloudFlare.CloudFlare(email=email, token=api_key, debug=debugging)
 
-    if resp.status_code == requests.codes.ok:
-        success = resp.json()['success']
-        if success:
+    request_params = {
+        'type': 'TXT',
+        'name': selector + '._domainkey.' + domain_suffix,
+        'content': data,
+        'ttl': ttl
+    }
+
+    try:
+        response = cf.zones.dns_records.post(zone_id, data=request_params)
+        if response:
             result = True
-        else:
-            result = False
-            logging.error( "DNS API cloudflare: failure:\n%s", resp.text )
-    else:
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
         result = False
-        logging.error( "DNS API cloudflare: HTTP error %d", resp.status_code )
-        logging.error( "DNS API cloudflare: error response body:\n%s", resp.text )
+        if len(e) > 0:
+            for ex in e:
+                logging.error('DNS API Cloudflare: [%d] %s', ex, ex)
+        else:
+            logging.error('DNS API Cloudflare: [%d] %s', e, e)
 
     return result
