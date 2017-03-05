c4b8eb8f76b8c39b9b76f4c4e1df01600c1fcd2d
Todd Knarr <tknarr@silverglass.org>
2017-03-04 22:52:12 -0800

    Put CloudFlare official API/SDK under it's own name, restore original requests-based module.

---
+++ cloudflareapi_list_zone_ids.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#    OpenDKIM genkeys - list zones and zone IDs for CloudFlare account
+#    Copyright (C) 2016 Todd Knarr <tknarr@silverglass.org>
+#    Copyright (C) 2017 Zachary Schneider (https://github.com/wackywired135)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import logging
+import sys
+import CloudFlare
+
+# Set up command-line argument parser and parse arguments
+parser = argparse.ArgumentParser(description="List CloudFlare zones and zone IDs")
+parser.add_argument("api_key", help="Global API key")
+parser.add_argument("email", help="Account email address")
+parser.add_argument("domain", nargs='?', default=None, help="Domain to request ID for")
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+api_key = args.api_key
+email = args.email
+if api_key == None:
+    logging.error("API key and email address are required.")
+    sys.exit(1)
+elif email == None:
+    logging.error("Email address is required.")
+    sys.exit(1)
+domain = args.domain
+
+cf = CloudFlare.CloudFlare(email=email, token=api_key, raw=True)
+
+zones = []
+current_page = 0
+total_pages = 1
+
+while current_page < total_pages:
+
+    current_page += 1
+    logging.info("Retrieving page %d of %d", current_page, total_pages)
+
+    request_params = {'per_page': 50, 'page': current_page, 'order': 'name', 'direction': 'asc'}
+    if domain:
+        request_params['name'] = domain
+
+    try:
+        response = cf.zones.get(params=request_params)
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
+        if len(e) > 0:
+            for ex in e:
+                logging.error('Cloudflare API: [%d] %s', ex, ex)
+        else:
+            logging.error('Cloudflare API: [%d] %s', e, e)
+        sys.exit(1)
+
+    result_info = response['result_info']
+
+    for zone in response['result']:
+        zones.append((zone['id'], zone['name']))
+
+    current_page = result_info['page']
+    total_pages = result_info['total_pages']
+
+for id, name in zones:
+    print("{0}\t{1}".format(id, name))
+
+sys.exit(0)
---
+++ dnsapi_cloudflareapi.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+#    OpenDKIM genkeys tool, CloudFlare API
+#    Copyright (C) 2016 Todd Knarr <tknarr@silverglass.org>
+#    Copyright (C) 2017 Zachary Schneider (https://github.com/wackywired135)
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Uses the 'python-cloudflare' package.
+
+# Requires:
+# dnsapi_data[0]        : Global API key
+# dnsapi_data[1]        : Email address
+# dnsapi_domain_data[0] : Zone ID
+# dnsapi_domain_data[1] : TTL in seconds, automatic if not specified
+# key_data['plain']     : TXT record value in plain unquoted format
+
+# Parameters:
+# type    : 'TXT'
+# name    : selector + '._domainkey.' + domain_suffix
+# content : key_data['plain']
+# ttl     : dnsapi_domain_data[1]
+
+import datetime
+import logging
+
+import CloudFlare
+
+
+def add(dnsapi_data, dnsapi_domain_data, key_data, debugging=False):
+    if len(dnsapi_data) < 2:
+        logging.error("DNS API Cloudflare: API credentials not configured")
+        return False,
+    api_key = dnsapi_data[0]
+    email = dnsapi_data[1]
+    if len(dnsapi_domain_data) < 1:
+        logging.error("DNS API Cloudflare: domain data does not contain zone ID")
+        return False,
+    zone_id = dnsapi_domain_data[0]
+    if len(dnsapi_domain_data) > 1:
+        try:
+            ttl = int(dnsapi_domain_data[1])
+            if ttl < 1:
+                ttl = 1
+        except Exception:
+            ttl = 1
+    else:
+        ttl = 1
+    try:
+        selector = key_data['selector']
+        data = key_data['plain']
+        domain_suffix = key_data['domain']
+    except KeyError as e:
+        logging.error("DNS API Cloudflare: required information not present: %s", str(e))
+        return False,
+    if debugging:
+        return True, key_data['domain'], selector
+
+    cf = CloudFlare.CloudFlare(email=email, token=api_key, debug=debugging)
+
+    request_params = {
+        'type': 'TXT',
+        'name': selector + '._domainkey.' + domain_suffix,
+        'content': data,
+        'ttl': ttl
+    }
+
+    try:
+        response = cf.zones.dns_records.post(zone_id, data=request_params)
+        if response:
+            # TODO need resource ID appended to result
+            result = True, key_data['domain'], selector, datetime.datetime.utcnow()
+        else:
+            result = False,
+    except CloudFlare.exceptions.CloudFlareAPIError as e:
+        result = False,
+        if len(e) > 0:
+            for ex in e:
+                logging.error('DNS API Cloudflare: [%d] %s', ex, ex)
+        else:
+            logging.error('DNS API Cloudflare: [%d] %s', e, e)
+
+    return result
+
+
+def delete(dnsapi_data, dnsapi_domain_data, record_data, debugging = False):
+    # TODO delete record
+    return None
